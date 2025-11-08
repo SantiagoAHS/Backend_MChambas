19df7f02058586be4fd1d73ec2dc5defcdfada8b
SantiagoAHS <lionelsanti2018@gmail.com>
2025-11-07 18:48:05 -0600

    Agregar requirements.txt y Procfile para Render v1.2

--- backendMc/settings.py
+++ backendMc/settings.py
@@ -30,13 +30,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-jj!)(=n!y^gt*c8#0n+)71sz!8rfn7w0escvdv59!q78ie@_zk'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-jj!)(=n!y^gt*c8#0n+)71sz!8rfn7w0escvdv59!q78ie@_zk')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = []
-
+# Allowed hosts
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
 
 # Application definition
 
